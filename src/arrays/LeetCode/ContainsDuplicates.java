3e33944dd2b4173213dd03eeedbed8b391317bf6
SparksAndCircuits <warrenandrade@live.com>
2025-05-25 20:32:51 -0400

    LeetCode DSA ContainsDuplicates;

---
+++ src/arrays/LeetCode/ContainsDuplicates.java
@@ -0,0 +1,24 @@
+package arrays.LeetCode;
+
+/* Write a program to check if an array is unique or not */
+
+public class ContainsDuplicates {
+    public boolean isUnique(int[] intArray) {
+        for (int i = 0; i < intArray.length; i++) {
+            for (int j = i + 1; j < intArray.length; j++) {
+                if (intArray[i] == intArray[j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ContainsDuplicates mn = new ContainsDuplicates();
+        int[] intArray = { 1, 2, 3, 4, 5, 6 };
+        boolean result = mn.isUnique(intArray);
+        System.out.println(result);
+    }
+}
