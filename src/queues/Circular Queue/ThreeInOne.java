577eaf962ea6da66a0a7cb9770707e4145c651d1
SparksAndCircuits <warrenandrade@live.com>
2025-06-06 07:10:27 -0400

    Updating ThreeInOne class

--- src/queues/Circular Queue/ThreeInOne.java
+++ src/queues/Circular Queue/ThreeInOne.java
@@ -5,24 +5,70 @@ public class ThreeInOne {
     private int[] sizes;
 
     public ThreeInOne(int stackSizes) {
-        stackSizes = -1;
+        this.capacity = stackSizes;
         values = new int[stackSizes * numberOfStacks];
         sizes = new int[numberOfStacks];
     }
 
+    // public boolean isFull(int stackNum) {
+    // if (sizes[stackNum] == stackSizes) {
+    // return true;
+    // } else {
+    // return false;
+    // }
+    // }
+
     public boolean isFull(int stackNum) {
-        if (sizes[stackNum] == stackSizes) {
-            return true;
+        return sizes[stackNum] == capacity;
+    }
+
+    // public boolean isEmpty(int stackNum) {
+    // if (sizes[stackNum == 0]) {
+    // return true;
+    // } else {
+    // return false;
+    // }
+    // }
+
+    public boolean isEmpty(int stackNum) {
+        return sizes[stackNum] == 0;
+    }
+
+    private int indexOfTop(int stackNum) {
+        int offset = stackNum * capacity;
+        int size = sizes[stackNum];
+        return offset + size - 1;
+    }
+
+    public void push(int stackNum, int value) {
+        if (isFull(stackNum)) {
+            System.out.println("The Stack is full.");
         } else {
-            return false;
+            sizes[stackNum]++;
+            values[indexOfTop(stackNum)] = value;
         }
     }
 
-    public boolean isEmpty(int stackNum) {
-        if (sizes[stackNum == 0]) {
-            return true;
+    public int pop(int stackNum) {
+        if (isEmpty(stackNum)) {
+            System.out.println("The stack is empty.");
+            return -1;
         } else {
-            return false;
+            int topIndex = indexOfTop(stackNum);
+            int value = values[topIndex];
+            values[topIndex] = 0;
+            sizes[stackNum]--;
+            return value;
         }
     }
+
+    public int peek(int stackNum) {
+        if (isEmpty(stackNum)) {
+            System.out.println("The stack is empty.");
+            return -1;
+        } else {
+            return values[indexOfTop(stackNum)];
+        }
+    }
+
 }
