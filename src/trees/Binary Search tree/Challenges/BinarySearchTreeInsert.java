f851ace237b8736cba84491a1172c9df3b240cc4
SparksAndCircuits <warrenandrade@live.com>
2025-06-09 18:02:32 -0400

    Binary search tree: Invert search tree Write am method to invert(or mirror) a binary tree. This means that for every node in the binary tree, you will swap the left and right children.

---
+++ src/trees/Binary Search tree/Challenges/BinarySearchTreeInsert.java
@@ -0,0 +1,76 @@
+/*
+ * Binary search tree: Invert search tree
+ * Write am method to invert(or mirror) a binary tree. This means that for every node
+ * in the binary tree, you will swap the left and right children.
+ */
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinarySearchTreeInsert {
+    private Node root;
+
+    public Node getRoot() {
+        return root;
+    }
+
+    private Node insert(Node currentNode, int value) {
+        if (currentNode == null) {
+            return new Node(value);
+        }
+
+        if (value < currentNode.value) {
+            currentNode.left = insert(currentNode.left, value);
+        } else if (value > currentNode.value) {
+            currentNode.right = insert(currentNode.right, value);
+        }
+
+        return currentNode;
+    }
+
+    public void insert(int value) {
+        if (root == null) {
+            root = new Node(value);
+        }
+
+        insert(root, value);
+    }
+
+    public ArrayList<Integer> BSF() {
+        Node currentNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> results = new ArrayList<>();
+
+        if (currentNode != null) {
+            queue.add(currentNode);
+        }
+
+        while (queue.size() > 0) {
+            currentNode = queue.remove();
+
+            if (currentNode != null) {
+                results.add(currentNode.value);
+                queue.add(currentNode.left);
+                queue.add(currentNode.right);
+            }
+        }
+
+        return results;
+    }
+
+    public void insert() {
+        root = invertTree(root);
+    }
+
+    private Node invertTree(Node node) {
+        if (node == null) {
+            return null;
+        }
+
+        Node temp = node.left;
+        node.left = invertTree(node.right);
+        node.right = invertTree(temp);
+        return node;
+    }
+}
