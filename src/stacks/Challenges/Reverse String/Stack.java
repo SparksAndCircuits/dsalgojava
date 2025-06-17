7c231c894540281b83e0bacec6c645c7d5da5c56
SparksAndCircuits <warrenandrade@live.com>
2025-06-16 20:35:10 -0400

    Adding some challenges for stack/queues

--- src/stacks/Challenges/Reverse String/Stack.java
+++ src/stacks/Challenges/Reverse String/Stack.java
@@ -64,4 +64,42 @@ public class Stack<T> {
 
         return stack.isEmpty();
     }
+
+    public static void sortStack(Stack<Integer> stack) {
+        Stack<Integer> additionalStack = new Stack<>();
+        while (!stack.isEmpty()) {
+            int temp = stack.pop();
+            ;
+
+            while (!additionalStack.isEmpty() && additionalStack.peek() > temp) {
+                stack.push(additionalStack.pop());
+            }
+
+            additionalStack.push(temp);
+        }
+
+        while (!additionalStack.isEmpty()) {
+            stack.push(additionalStack.pop());
+        }
+    }
+
+    public void enqueue(int value) {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+
+        stack1.push(value);
+
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+    }
+
+    public Integer dequeue() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            return stack1.pop();
+        }
+    }
 }
