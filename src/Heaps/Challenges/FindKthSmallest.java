3cd97892e46092558b26223c92a17ba2eab7b3e9
SparksAndCircuits <warrenandrade@live.com>
2025-06-16 21:21:35 -0400

    /*      * Implement a function name stream max that processes a stream of integers and      * returns the max number encountered so far for each input number.      * This function should take in an array of integers and return a list of      * integers given an array of integers, your function should iterate over the      * array and for each number it should find the max number that number.      */

--- src/Heaps/Challenges/FindKthSmallest.java
+++ src/Heaps/Challenges/FindKthSmallest.java
@@ -8,6 +8,11 @@
  * the array were sorted in ascending order. the elements at the kth postion
  * (1-indexed) is the output
  */
+
+import java.util.ArrayList;
+
+import java.util.List;
+
 public class FindKthSmallest {
     public static int findKthSmallest(int[] nums, int k) {
         Heap maxHeap = new Heap();
@@ -22,4 +27,24 @@ public class FindKthSmallest {
 
         return maxHeap.remove();
     }
+
+    /*
+     * Implement a function name stream max that processes a stream of integers and
+     * returns the max number encountered so far for each input number.
+     * This function should take in an array of integers and return a list of
+     * integers given an array of integers, your function should iterate over the
+     * array and for each number it should find the max number that number.
+     */
+
+    public static List<Integer> streamMax(int[] nums) {
+        Heap maxHeap = new Heap();
+        List<Integer> maxStream = new ArrayList<>();
+
+        for (int num : nums) {
+            maxHeap.insert(num);
+            maxStream.add(maxHeap.getHeap().get(0));
+        }
+    
+        return maxStream;
+     }
 }
