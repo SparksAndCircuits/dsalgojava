0147f0bdf48ea3f24718bef2099fa6e38e0f4caf
SparksAndCircuits <warrenandrade@live.com>
2025-06-22 21:04:40 -0400

    Basic Binary Tree

---
+++ src/trees/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        TreeNode drinks = new TreeNode("Drinks");
+        TreeNode hot = new TreeNode("Hot");
+        TreeNode cold = new TreeNode("Cold");
+        drinks.addChild(hot);
+        drinks.addChild(cold);
+        System.out.println(drinks.print(0));
+    }
+}
