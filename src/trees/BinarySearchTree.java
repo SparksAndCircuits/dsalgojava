f89ee16d9ad756cd029511de226853f9e3b5f99a
SparksAndCircuits <warrenandrade@live.com>
2025-06-25 21:08:46 -0400

    Updating Binary Search tree

--- src/trees/BinarySearchTree.java
+++ src/trees/BinarySearchTree.java
@@ -1,39 +1,42 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinarySearchTree {
     BinaryNode root;
 
-    BinarySearchTree(){
+    BinarySearchTree() {
         root = null;
     }
 
-    private BinaryNode insert(BinaryNode currentNode, int value){
-        if(currentNode == null){
+    private BinaryNode insert(BinaryNode currentNode, int value) {
+        if (currentNode == null) {
             BinaryNode newNode = new BinaryNode();
             newNode.value = value;
             System.out.println("The value was successfully inserted into the Binary Search Tree.");
             return newNode;
-        }else if(value <= currentNode.value){
+        } else if (value <= currentNode.value) {
             currentNode.left = insert(currentNode.left, value);
             return currentNode;
-        }else{
+        } else {
             currentNode.right = insert(currentNode.right, value);
             return currentNode;
         }
     }
 
-    void insert(int value){
+    void insert(int value) {
         insert(root, value);
     }
 
-    public void preOrder(BinaryNode node){
-        if(node == null){
+    public void preOrder(BinaryNode node) {
+        if (node == null) {
             return;
         }
         System.out.println(node.value + " ");
         preOrder(node.left);
     }
 
-    public void inOrder(BinaryNode node){
-        if(node == null){
+    public void inOrder(BinaryNode node) {
+        if (node == null) {
             return;
         }
 
@@ -42,12 +45,75 @@ public class BinarySearchTree {
         inOrder(node.right);
     }
 
-    public void postOrder(BinaryNode node){
-        if(node == null){
+    public void postOrder(BinaryNode node) {
+        if (node == null) {
             return;
         }
 
         postOrder(node.left);
         postOrder(node.right);
+    }
+
+    public void levelOrder() {
+        Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            BinaryNode presentNode = queue.remove();
+            System.out.println(presentNode.value + " ");
+            if (presentNode.left != null) {
+                queue.add(presentNode.left);
+            }
+
+            if (presentNode.right != null) {
+                queue.add(presentNode.right);
+            }
+        }
+    }
+
+    BinaryNode search(BinaryNode node, int value) {
+        if (node == null) {
+            System.out.println("Value: " + value + " was not found in the Binary Search tree.");
+            return null;
+        } else if (node.value == value) {
+            System.out.println("Value: " + value + " was found in the binary search tree");
+            return node;
+        } else if (value < node.value) {
+            return search(node.left, value);
+        } else {
+            return search(node.right, value);
+        }
+    }
+
+    public BinaryNode deleteNode(BinaryNode root, int value) {
+        if (root == null) {
+            System.out.println("Value was not found in the binary search tree.");
+            return null;
+        }
+
+        if (value < root.value) {
+            root.value = deleteNode(root.value, value);
+        } else if (value > root.value) {
+            root.value = deleteNode(root.value, value);
+        } else {
+            if (root.left != null && root.right != null) {
+                BinaryNode temp = root;
+                BinaryNode minNodeForRight = minimumNode(temp.right);
+                root.value = minNodeForRight.value;
+                root.right = deleteNode(root.right, minNodeForRight.value);
+            } else if (root.left != null) {
+                root = root.left;
+            } else if (root.right != null) {
+                root = root.right;
+            } else {
+                root = null;
+            }
+        }
+
+        return root;
+    }
+
+    public void deleteBST() {
+        root = null;
+        System.out.println("The Binary search tree has been deleted.");
     }
 }
