3b9bd03320e3e56dc3c3ffbb5372717a3fe4a3b1
SparksAndCircuits <warrenandrade@live.com>
2025-08-14 18:36:58 -0400

    Swap Nodes in Pairs Doubly Linked List interview questions.

---
+++ src/linkedlists/Doubly Linked Lists/Interview Questions/Swap nodes in Pairs/DoublyLinkedList.java
@@ -0,0 +1,92 @@
+/*
+ *  -   You are given a doubly linked list,
+ *  -   Implement a method called swapPairs within the class that swaps the values of adjacent nodes in the linked list.The method should not ake any input 
+ *      parameters.
+ *  -   Note: This doubly linked list does not have a tail pointer which makes the implementation easier.
+ *  -   Also Note: There is not a length attribute.
+ */
+public class DoublyLinkedList {
+    private Node head;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+    }
+
+    public void printList() {
+        StringBuilder output = new StringBuilder();
+        Node temp = head;
+        while (temp != null) {
+            output.append(temp.value);
+            if (temp.next != null) {
+                output.append(" <-> ");
+            }
+
+            temp = temp.next;
+        }
+
+        System.out.println(output.toString());
+    }
+
+    public void makeEmpty() {
+        head = null;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = newNode;
+            newNode.prev = current;
+        }
+    }
+
+    public void swapPairs() {
+        Node dummyNode = new Node(0);
+        dummyNode.next = head;
+        Node previousNode = dummyNode;
+
+        while (head != null && head.next != null) {
+            Node firstNode = head;
+            Node secondNode = head.next;
+
+            previousNode.next = secondNode;
+            firstNode.next = secondNode.next;
+            secondNode.next = firstNode;
+
+            secondNode.prev = previousNode;
+            firstNode.prev = secondNode;
+
+            if (firstNode.next != null) {
+                firstNode.next.prev = firstNode;
+            }
+
+            head = firstNode.next;
+            previousNode = firstNode;
+        }
+
+        head = dummyNode.next;
+        if (head != null) {
+            head.prev = null;
+        }
+    }
+}
