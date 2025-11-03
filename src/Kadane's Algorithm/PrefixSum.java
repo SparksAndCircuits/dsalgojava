df784994a2438f5deb3d710648c00e7a49268bd9
SparksAndCircuits <warrenandrade@live.com>
2025-11-02 22:32:01 -0500

    Prefix Sum addition

--- src/Kadane's Algorithm/PrefixSum.java
+++ src/Kadane's Algorithm/PrefixSum.java
@@ -33,13 +33,56 @@ public class PrefixSum {
         }
     }
 
-    public static void demonstrateRangeSumQuery(){
-        int[] array = {3, 1, 4, 1, 5, 9, 2, 6};
+    public static void demonstrateRangeSumQuery() {
+        int[] array = { 3, 1, 4, 1, 5, 9, 2, 6 };
         RangeSumQuery rsq = new RangeSumQuery(array);
         rsq.sumRange(2, 5);
         rsq.sumRange(0, 3);
         rsq.sumRange(4, 7);
     }
 
-    
+    // Problem 2: Subarray sum equals k
+    public static int subarraySumEqualsK(int[] array, int k) {
+        System.out.println("\n--- Problem 2: Subarray sum equals k ---");
+        System.out.println("Array: " + Arrays.toString(array));
+        System.out.println("Target K: " + k);
+        System.out.println("\nStep-by-Step process:");
+
+        Map<Integer, Integer> prefixCount = new HashMap<>();
+        prefixCount.put(0, 1); // Empty prefix
+
+        int count = 0;
+        int currentSum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            currentSum += array[i];
+
+            System.out.println("\nIndex " + i + ": array[" + i + "] = " + array[i]);
+            System.out.println("CurrentSum = " + currentSum);
+
+            int target = currentSum - k;
+            System.out.println("Looking for prefix sum = " + target + " (currentSum - k)");
+
+            if (prefixCount.containsKey(target)) {
+                int occurrences = prefixCount.get(target);
+                count += occurrences;
+                System.out.println("Found " + occurrences + " occurrence(s)!");
+                System.out.println("Total count now = " + count);
+            } else {
+                System.out.println("Not found!");
+            }
+
+            prefixCount.put(currentSum, prefixCount.getOrDefault(currentSum, 0) + 1);
+            System.out.println("Updated map: " + prefixCount);
+        }
+
+        System.out.println("\nTotal subarrays with sum = " + k + ": " + count);
+        return count;
+    }
+
+    public static void demonstrateSubarraySum() {
+        int[] array = { 1, 2, 3, -3, 1, 1, 1, 4, 2, -3 };
+        int k = 3;
+        subarraySumEqualsK(array, k);
+    }
 }
